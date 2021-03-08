292f220090bbe1f4c1b0753d32836f0da27416e7
Addis Belete <addis.bel@gmail.com>
2021-03-08 21:34:41 +0300

    add and style register class

--- script.js
+++ script.js
@@ -18,11 +18,3 @@ function myFunction() {
   }
 }
 
-function ourFunction() {
-  var x = document.getElementById("myUL");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
